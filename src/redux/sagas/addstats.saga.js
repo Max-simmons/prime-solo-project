ef9dd5aa2b164237c79d30b392c7f41417672160
Max Simmons <maxsimmons20@gmail.com>
2023-05-23 10:36:04 -0500

    added post to sagas

--- src/redux/sagas/addstats.saga.js
+++ src/redux/sagas/addstats.saga.js
@@ -1,2 +1,19 @@
 import { takeLatest, put } from "redux-saga/effects";
 import axios from "axios";
+
+function* addGame(action) {
+    try{
+        const dbResponse = yield axios.post('/api/stats', action.payload)
+        console.log ('got the req', action.payload);
+
+        yield put({type: 'SAGA/FETCH_STATS'})
+    } catch {
+        console.log('error in posting');
+    }
+}
+
+function* sagaAddGame() {
+    yield takeLatest('SAGA/ADD_GAME', addGame)
+}
+
+export default sagaAddGame;
