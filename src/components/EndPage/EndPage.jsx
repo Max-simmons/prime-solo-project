4b2cd54f229b88a3fd5a9b9f1c05e8c4f9e12eed
Max Simmons <maxsimmons20@gmail.com>
2023-06-18 16:19:34 -0500

    updated end page

--- src/components/EndPage/EndPage.jsx
+++ src/components/EndPage/EndPage.jsx
@@ -17,7 +17,7 @@ return (
     <Container>
 
     <p>I would like to thank my friends and family, I would also like to thank 
-        all the the Prime staff and especially my insturctors Matt and Andrew.
+        all the the Prime staff and especially to my insturctors Matt and Andrew.
     </p>
     <h3>Technology Used</h3>
     <ul>
